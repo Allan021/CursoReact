b19f6af806a1a1a077fb8caf95e659959179b005
Allan Catro <allancastro1912@gmail.com>
2020-10-20 14:42:02 -0600

    Modificando el archivo courses

--- src/Components/Pages/Course.jsx
+++ src/Components/Pages/Course.jsx
@@ -1,32 +1,63 @@
-import React,{Component},{useState} from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
 
-class Course extends Component {
+const courses = [
 
-    constructor(props){
-        super(props)
-this.state={/*
-    El match viene por propiedades por lo que el me obtiene los parametros de cada uno de lo cursos*/    CursoActual : {}/*Esto es un objeto porque aqui solo impirmire un elemento a la vez y no impirmire la grilla de cursos sino un solo elemento*/
-}
-    }
-    componentDidMount(){
-        axios.get(`http://my-json-server.typicode.com/Allan021/JSONDB/cursos/${this.state.match.params.id}`)
-        .then(response => this.setState({
-            CursoActual : response.data
-        }))
-    
+        {
+            "id": 1,
+        "image" : "https://edteam-media.s3.amazonaws.com/courses/small/61e5a210-8dab-412e-a6dc-802c070cc18c.jpg",
+        "title" : "Java Script desde Cero",
+        "teacher": "Allan Castro",
+        "price" : 60,
+        "description" : "Domina el mundo de JavaScript en tiempo record"
+        },
+        
+        {
+            "id": 2,
+            "image" : "https://edteam-media.s3.amazonaws.com/courses/small/26557907-0555-427e-a40c-6ff207f98d72.png",
+        "title" : "Html Desde Cero",
+        "teacher": "Paola Diaz",
+        "price" : 20,
+        "description" : "Domina el mundo de Html 5 en tiempo record"
+        },
+        
+        {    
+            "id": 3,
+        "image" : "https://edteam-media.s3.amazonaws.com/courses/small/084a20f8-e327-47ce-a073-1df1f90e4a74.jpg",
+        "title" : "Responsive Web Apps",
+        "teacher": "Karol cacerers",
+        "price" : 50,
+        "description" : "Domina las responsive Web Aps en tiempo record"
+        },
+        {  
+        
+            "id": 4,
+            "image" : "https://edteam-media.s3.amazonaws.com/courses/small/262e68b3-eea2-43f2-9e9a-febc106a8295.png",
+        "title" : "Css desde Cero",
+        "teacher": "Alvaro Felipe",
+        "price" : 30,
+        "description" : "Domina el mundo de Css en tiempo record"
         }
-    
-  render(){  
+        
+]
+
+
+
+
+const Course = (match) => {
+
+  
+const CursoActual = courses.filter( course => course.id === parseInt(match.params.id))[0]/*El filter me sirve para filtrar el coontenido pero dependiendo de una condicion y el mathc me sirve pafra extraer los parametros de una url*/    
+  
 return (
 <div className="l-section s-py-4">
     <div className="ed-grid lg-grid-2">
     {
-    this.state.CursoActual? (
+    CursoActual? (
       <>
-        <h1 className="lg-cols-1">{this.CursoActual.titulo}</h1>
-<img className="lg-cols-3" src={this.CursoActual.imagen}alt={this.CursoActual.titulo} />
-<p className="lg-cols-3">Maestro: {this.CursoActual.maestro}</p>
+        <h1 className="lg-cols-1">{CursoActual.titulo}</h1>
+<img className="lg-cols-3" src={CursoActual.imagen}alt={CursoActual.titulo} />
+<p className="lg-cols-3">Maestro: {CursoActual.maestro}</p>
 </>
 
     ) : (
@@ -43,7 +74,9 @@ return (
 )
 
 }
-}
+
+
+
 
 export default Course
 
